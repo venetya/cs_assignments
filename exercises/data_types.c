48b00a4dc3eb2bc9b54413418bd0030341f7b2ff
venetyaevans <venetya.evans@regencylighting.com>
2017-11-06 08:56:45 -0800

    corrected 'bits' to 'bytes'

--- exercises/data_types.c
+++ exercises/data_types.c
@@ -6,9 +6,10 @@ int main(){
     int i = 7;
     long l = 10;
 
-    printf("char %c has a size of %lu bit\n", x, sizeof(x));
-    printf("short %i has a size of %lu bits\n", s, sizeof(s));
-    printf("int %d has a size of %lu bits\n", i, sizeof(i));
-    printf("long %li has a size of %lu bits\n", l, sizeof(l));
+    printf("char %c has a size of %lu bytes\n", x, sizeof(x));
+    printf("short %i has a size of %lu bytes\n", s, sizeof(s));
+    printf("int %d has a size of %lu bytes\n", i, sizeof(i));
+    printf("long %li has a size of %lu bytes\n", l, sizeof(l));
+
     return 0;
 }
